8d762bdd1707ed809142371834c6c70e2d0a28d3
devpedroval <prmval@gmail.com>
2023-09-01 14:53:59 -0300

    requisito 1 completo

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,16 @@
 from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Restriction
+
+ANIMAL_MEAT = 'ANIMAL_MEAT'
+ANIMAL_DERIVED = 'ANIMAL_DERIVED'
 
 
-# Req 1
 def test_ingredient():
-    pass
+    ingrediente = Ingredient("bacon")
+    assert ingrediente.__hash__() == hash('bacon')
+    assert ingrediente.__eq__(ingrediente) is True
+    assert ingrediente.__eq__("bacon") is False
+    assert ingrediente.__repr__() == "Ingredient('bacon')"
+    assert ingrediente.name == "bacon"
+    assert ingrediente.restrictions == {Restriction.ANIMAL_MEAT,
+                                        Restriction.ANIMAL_DERIVED}
